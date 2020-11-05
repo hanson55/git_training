1e949cb0bd79a7f868ab22f27794c1062d7379bf
hanson55 <jose.hanson.7e4@itb.cat>
2020-11-05 11:58:04 +0100

    java script

---
+++ If.java
@@ -0,0 +1,11 @@
+public class If{
+	public static void main(String[] args){
+		boolean a = false; 
+		if(a == true){
+			System.out.println("Hola");
+}
+		else {
+			System.out.println("Adeu");
+		}
+	}
+}
