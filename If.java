f4b67d608cb3b739fb03fbbae2b814a1df8338a8
hanson55 <72549752+hanson55@users.noreply.github.com>
2020-12-03 11:25:46 +0100

    Update If.java

--- If.java
+++ If.java
@@ -1,12 +1,10 @@
 public class If{
 	public static void main(String[] args){
-		//
-		boolean a = false; 
-		if(a == true){
-			System.out.println("Hola");
-		}
-		else {
-			System.out.println("Adeu");
-		}
+		//Como indicar el numero y la posicion del array con float
+		float [] values = new float [10];
+        	values[9] = 2000;
+        	values[2] = 4;
+        	System.out.println(Arrays.toString(values));
+		
 	}
 }
